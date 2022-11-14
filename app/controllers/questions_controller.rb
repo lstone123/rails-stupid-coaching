1d2ef06633b82460f539783da958ca07b480bc6e
Louise Stone <louisemstone@hotmail.com>
2022-11-14 14:30:36 +0000

    added styling

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,10 +4,10 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    if @question === "I am going to work"
-      @answer = "Great!"
-    elsif @question.include?"?"
-      @answer = "Silly question, get dressed and go to work!"
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
     end
